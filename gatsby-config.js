5b5a50b77199fbc2027b82519b05006038e4a81f
coupong <59623602+jp-prime@users.noreply.github.com>
2026-06-25 09:20:03 +0700

    Initial commit: gatsby blog setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,24 @@ module.exports = {
     title: `My vinatong blog`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // TODO: 실제 Google Analytics 트래킹 ID가 있다면 아래에 입력해주세요. (예: "G-XXXXXXXXXX")
+        trackingIds: [
+          "G-TRACKING_ID",
+        ],
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    }, "gatsby-plugin-mdx", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
